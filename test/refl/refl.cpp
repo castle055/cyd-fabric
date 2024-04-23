3d883c304dbbadcf57072d5647df2d94ef0fab3c
Victor Castillo <victorcastilloaguero@gmail.com>
2024-04-23 17:27:05 +0200

    update

--- test/refl/refl.cpp
+++ test/refl/refl.cpp
@@ -1,4 +1,20 @@
-// Copyright (c) 2024, Victor Castillo, All rights reserved.
+/*!
+ ! Copyright (c) 2024, Víctor Castillo Agüero.
+ ! This file is part of the Cydonia project.
+ !
+ ! This library is free software: you can redistribute it and/or modify
+ ! it under the terms of the GNU General Public License as published by
+ ! the Free Software Foundation, either version 3 of the License, or
+ ! (at your option) any later version.
+ !
+ ! This library is distributed in the hope that it will be useful,
+ ! but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ! GNU General Public License for more details.
+ !
+ ! You should have received a copy of the GNU General Public License
+ ! along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <cassert>
 
@@ -75,12 +91,12 @@ TEST("Nominal Start-up2") (
   }
 
   print_quantity("TOTAL", sum);
-
+  auto rs = std::ranges::sort(prices, std::ranges::greater{}, [](auto&& it){ return 1/it; });
   sum = 0;
-  std::ranges::sort(prices);
-  for (int p: prices) {
+  for (const int& p: prices) {
     sum += p;
-    print_quantity("", sum);
+    // print_quantity("", sum);
+    print_quantity("", p);
   }
 
   return 0;
