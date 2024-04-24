d17da6ae23fe4ffed59a2d53a7b06622c34e99a3
Victor Castillo <victorcastilloaguero@gmail.com>
2024-04-24 14:00:38 +0200

    units: added some more reduce rules

--- include/cyd_fabric/units/reduce_rules.h
+++ include/cyd_fabric/units/reduce_rules.h
@@ -1,4 +1,20 @@
-// Copyright (c) 2024, Victor Castillo, All rights reserved.
+/*!
+ ! Copyright (c) 2024, Víctor Castillo Agüero.
+ ! This file is part of the Cydonia project.
+ !
+ ! This library is free software: you can redistribute it and/or modify
+ ! it under the terms of the GNU General Public License as published by
+ ! the Free Software Foundation, either version 3 of the License, or
+ ! (at your option) any later version.
+ !
+ ! This library is distributed in the hope that it will be useful,
+ ! but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ! GNU General Public License for more details.
+ !
+ ! You should have received a copy of the GNU General Public License
+ ! along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef CYD_FABRIC_REDUCE_RULES_H
 #define CYD_FABRIC_REDUCE_RULES_H
@@ -21,9 +37,25 @@ namespace cyd::fabric::units {
   struct frac<Numerator,Numerator> {
     using reduce = no_unit;
   };
+  template <typename N>
+  struct frac<N, no_unit> {
+    using reduce = typename N::reduce;
+  };
+  template <typename N>
+  struct frac<no_unit, N> {
+    using reduce = typename N::reduce;
+  };
   
   
   
+  template <typename P>
+  struct mul<P, no_unit> {
+    using reduce = typename P::reduce;
+  };
+  template <typename P>
+  struct mul<no_unit, P> {
+    using reduce = typename P::reduce;
+  };
   template <typename P1, typename P2>
   struct mul<frac<P1,P2>,P2> {
     using reduce = typename P1::reduce;
