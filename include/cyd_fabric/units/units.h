5b701a7de70dd043782171696c47c19a2bcbc236
Victor Castillo <victorcastilloaguero@gmail.com>
2024-04-27 01:04:01 +0200

    units::quantity_t: added no argument default constructor

--- include/cyd_fabric/units/units.h
+++ include/cyd_fabric/units/units.h
@@ -30,130 +30,140 @@ namespace cyd::fabric::units {
   struct no_unit {
     using reduce = no_unit;
   };
-  
-  template <typename U_FROM, typename U_TO, typename T>
-  struct unit_conversion_t {};
-  
-  template <typename U_FROM, typename U_TO, typename T>
-  concept Convertible = requires { unit_conversion_t<U_FROM,U_TO,T>::factor; }
-                        || requires { unit_conversion_t<U_TO,U_FROM,T>::factor; };
+
+  template<typename U_FROM, typename U_TO, typename T>
+  struct unit_conversion_t {
+  };
+
+  template<typename U_FROM, typename U_TO, typename T>
+  concept Convertible = requires { unit_conversion_t<U_FROM, U_TO, T>::factor; }
+                        || requires { unit_conversion_t<U_TO, U_FROM, T>::factor; };
 
 
 #define UNIT_SYMBOL(...) static constexpr inline std::string symbol() noexcept { return (__VA_ARGS__); }
-  
+
   template<typename Numerator, typename Denominator>
   struct frac {
-    using scale = frac<typename Numerator::scale, typename Denominator::scale>;
+    using scale  = frac<typename Numerator::scale, typename Denominator::scale>;
     using reduce = frac<typename Numerator::reduce, typename Denominator::reduce>;
-    
-    template <typename T>
+
+    template<typename T>
     using factor = ratio<
       T,
       (long)(Numerator::template factor<T>::denominator * Denominator::template factor<T>::numerator),
       (long)(Numerator::template factor<T>::numerator * Denominator::template factor<T>::denominator)
     >;
-    
+
     UNIT_SYMBOL("(" + Numerator::symbol() + ")/(" + Denominator::symbol() + ")")
   };
-  
-  template<typename Product1, typename ...Products>
+
+  template<typename Product1, typename... Products>
   struct mul {
-    using scale = mul<typename Product1::scale, typename Products::scale...>;
+    using scale  = mul<typename Product1::scale, typename Products::scale...>;
     using reduce = mul<typename Product1::reduce, typename Products::reduce...>;
-    
-    template <typename T>
+
+    template<typename T>
     using factor = ratio<
       T,
       (long)(Product1::template factor<T>::denominator * (... * Products::template factor<T>::denominator)),
       (long)(Product1::template factor<T>::numerator * (... * Products::template factor<T>::numerator))
     >;
-    
+
     UNIT_SYMBOL(Product1::symbol() + (... + ("*" + Products::symbol())))
   };
-  
-  template <typename U1, typename U2>
+
+  template<typename U1, typename U2>
   concept SameScale = std::same_as<typename U1::scale, typename U2::scale>;
-  
-  template <typename Q, typename S>
+
+  template<typename Q, typename S>
   concept Quantity = std::same_as<typename Q::unit::scale, S>;
-  
-  template <typename U, typename T>
+
+  template<typename U, typename T>
   struct quantity_t {
-    using unit = U;
+    using unit      = U;
     using data_type = T;
-    
-    T value{};
-    
-    template <typename U1>
-    quantity_t<typename frac<U,U1>::reduce,T> operator/(const quantity_t<U1,T>& rhl) const {
-      return {this->value/rhl.value};
+
+    T value { };
+
+    template<typename U1>
+    quantity_t<typename frac<U, U1>::reduce, T> operator/(const quantity_t<U1, T> &rhl) const {
+      return {this->value / rhl.value};
     }
-    
-    template <typename U1>
-    requires SameScale<U,U1>
-    quantity_t<typename mul<U,U>::reduce,T> operator*(const quantity_t<U1,T>& rhl) const {
+
+    template<typename U1>
+      requires SameScale<U, U1>
+    quantity_t<typename mul<U, U>::reduce, T> operator*(const quantity_t<U1, T> &rhl) const {
       return {this->value * rhl.template as<U>().value};
     }
-    
-    template <typename U1>
-    requires (!SameScale<U,U1>)
-    quantity_t<typename mul<U,U1>::reduce,T> operator*(const quantity_t<U1,T>& rhl) const {
+
+    template<typename U1>
+      requires (!SameScale<U, U1>)
+    quantity_t<typename mul<U, U1>::reduce, T> operator*(const quantity_t<U1, T> &rhl) const {
       return {this->value * rhl.value};
     }
-    
-    template <typename U1, typename T1>
-    requires (SameScale<U,U1>)
-    quantity_t<typename U::reduce,T1> operator+(const quantity_t<U1,T1>& rhl) const {
+
+    template<typename U1, typename T1>
+      requires (SameScale<U, U1>)
+    quantity_t<typename U::reduce, T1> operator+(const quantity_t<U1, T1> &rhl) const {
       return {this->value + rhl.template as<U>().value};
     }
-    
-    template <typename U1, typename T1>
-    requires (SameScale<U,U1>)
-    quantity_t<typename U::reduce,T1> operator-(const quantity_t<U1,T1>& rhl) const {
+
+    template<typename U1, typename T1>
+      requires (SameScale<U, U1>)
+    quantity_t<typename U::reduce, T1> operator-(const quantity_t<U1, T1> &rhl) const {
       return {this->value - rhl.template as<U>().value};
     }
-    
-    quantity_t(T value_): value(value_) {}
-    quantity_t(const quantity_t& other) {
+
+    quantity_t() = default;
+
+    quantity_t(T value_): value(value_) {
+    }
+
+    quantity_t(const quantity_t &other) {
       this->value = other.value;
     }
-    quantity_t(quantity_t&& other) noexcept {
+
+    quantity_t(quantity_t &&other) noexcept {
       this->value = other.value;
     }
-    quantity_t& operator=(const quantity_t& rhl) {
+
+    quantity_t &operator=(const quantity_t &rhl) {
       this->value = rhl.value;
       return *this;
     }
-    quantity_t& operator=(quantity_t&& rhl) noexcept {
+
+    quantity_t &operator=(quantity_t &&rhl) noexcept {
       this->value = rhl.value;
       return *this;
     }
-    
-    
-    template <typename U1>
-    requires SameScale<U,U1> || Convertible<U,U1,T>
+
+
+    template<typename U1>
+      requires SameScale<U, U1> || Convertible<U, U1, T>
     [[nodiscard]]
     quantity_t<U1, T> as() const {
-      if constexpr (std::is_same_v<U,U1>) {
+      if constexpr (std::is_same_v<U, U1>) {
         return *this;
-      } else if constexpr (SameScale<U,U1>) {
-        return quantity_t<U1,T>{this->value
-                                * (U1::template factor<T>::denominator
-                                   * U::template factor<T>::numerator)
-                                / (U1::template factor<T>::numerator
-                                   * U::template factor<T>::denominator)};
-      } else if constexpr (requires { unit_conversion_t<U,U1,T>::factor; }) {
-        return {this->value * unit_conversion_t<U,U1,T>::factor};
-      } else if constexpr (requires { unit_conversion_t<U1,U,T>::factor; }) {
-        return {this->value / unit_conversion_t<U1,U,T>::factor};
+      } else if constexpr (SameScale<U, U1>) {
+        return quantity_t<U1, T> {
+          this->value
+          * (U1::template factor<T>::denominator
+             * U::template factor<T>::numerator)
+          / (U1::template factor<T>::numerator
+             * U::template factor<T>::denominator)
+        };
+      } else if constexpr (requires { unit_conversion_t<U, U1, T>::factor; }) {
+        return {this->value * unit_conversion_t<U, U1, T>::factor};
+      } else if constexpr (requires { unit_conversion_t<U1, U, T>::factor; }) {
+        return {this->value / unit_conversion_t<U1, U, T>::factor};
       } else {
         static_assert(false, "No conversion factor available for these units.");
       }
     }
-    
+
     [[nodiscard]]
     inline std::string to_string() const noexcept {
-      std::string str{};
+      std::string str { };
       str.append(std::to_string(value));
       str.append(" [");
       str.append(U::symbol());
@@ -166,7 +176,7 @@ namespace cyd::fabric::units {
 }
 
 template<typename U, typename T>
-std::ostream & operator<<(std::ostream &o, const cyd::fabric::units::quantity_t<U,T>& quantity) {
+std::ostream &operator<<(std::ostream &o, const cyd::fabric::units::quantity_t<U, T> &quantity) {
   o << quantity.to_string();
   return o;
 }
@@ -176,8 +186,7 @@ namespace NAME {         \
   struct scale {};       \
   template <typename Q> concept quantity = Quantity<Q, scale>; \
 }                        \
-namespace NAME __VA_ARGS__            \
-
+namespace NAME __VA_ARGS__
 #define UNIT(NAME, SYMBOL, FACTOR_NUM, FACTOR_DEN) \
 struct NAME {                                      \
   using scale = scale;                             \
