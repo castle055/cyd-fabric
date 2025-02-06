77fa3838edb8bf2e827ebdd290cb632eb523e13c
Victor Castillo <victorcastilloaguero@gmail.com>
2025-02-06 01:18:43 +0100

    clean up

--- include/cyd_fabric/config/config.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2024, Víctor Castillo Agüero.
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#endif //CONFIG_H
--- include/cyd_fabric/ts/concepts.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2024, Víctor Castillo Agüero.
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#ifndef CONCEPTS_H
-#define CONCEPTS_H
-
-#endif //CONCEPTS_H
